adaacaf0b3a2980a00984437c83d5647e8da36f6
melmiranda <melania.smc@hotmail.com>
2017-04-20 12:25:58 -0300

    12:25 20-04-2017

--- wp-content/themes/baskerville-child/archive.php
+++ wp-content/themes/baskerville-child/archive.php
@@ -3,7 +3,7 @@
 <div class="wrapper section medium-padding">
 
 	<div class="page-title section-inner">
-		
+
 		<?php if ( is_day() ) : ?>
 			<h5><?php _e( 'Date', 'baskerville' ); ?></h5> <h3><?php echo get_the_date(); ?></h3>
 		<?php elseif ( is_month() ) : ?>
@@ -11,7 +11,8 @@
 		<?php elseif ( is_year() ) : ?>
 			<h5><?php _e( 'Year', 'baskerville' ); ?></h5> <h3><?php echo get_the_date('Y'); ?></h3>
 		<?php elseif ( is_category() ) : ?>
-			<h5><?php _e( 'Category', 'baskerville' ); ?></h5> <h3><?php echo single_cat_title( '', false ); ?></h3>
+			<!-- <h5><?php _e( 'Category', 'baskerville' ); ?></h5>
+			 <h3><?php echo single_cat_title( '', false ); ?></h3> -->
 		<?php elseif ( is_tag() ) : ?>
 			<h5><?php _e( 'Tag', 'baskerville' ); ?></h5> <h3><?php echo single_tag_title( '', false ); ?></h3>
 		<?php elseif ( is_author() ) : ?>
@@ -20,59 +21,60 @@
 		<?php else : ?>
 			<h5><?php _e( 'Archive', 'baskerville' ); ?></h5>
 		<?php endif; ?>
-		
+
 		<?php
 			$tag_description = tag_description();
 			if ( ! empty( $tag_description ) )
 				echo apply_filters( 'tag_archive_meta', '<div class="tag-archive-meta">' . $tag_description . '</div>' );
 		?>
-		
-	</div> <!-- /page-title -->
-	
+
+	</div>
+	 <!-- /page-title -->
+
 	<div class="content section-inner">
-	
+
 		<?php if ( have_posts() ) : ?>
-	
+
 			<div class="posts">
-			
+
 				<?php rewind_posts(); ?>
-			
+
 				<?php while ( have_posts() ) : the_post(); ?>
-				
+
 					<div class="post-container">
-				
+
 						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				
+
 							<?php get_template_part( 'content', get_post_format() ); ?>
-							
+
 							<div class="clear"></div>
-							
+
 						</div> <!-- /post -->
-					
+
 					</div>
-					
+
 				<?php endwhile; ?>
-							
+
 			</div> <!-- /posts -->
-						
+
 			<?php if ( $wp_query->max_num_pages > 1 ) : ?>
-			
+
 				<div class="archive-nav">
-				
+
 					<?php echo get_next_posts_link( '&laquo; ' . __('Older posts', 'baskerville')); ?>
-						
+
 					<?php echo get_previous_posts_link( __('Newer posts', 'baskerville') . ' &raquo;'); ?>
-					
+
 					<div class="clear"></div>
-					
+
 				</div> <!-- /post-nav archive-nav -->
-				
+
 				<div class="clear"></div>
-				
+
 			<?php endif; ?>
-					
+
 		<?php endif; ?>
-	
+
 	</div> <!-- /content -->
 
 </div> <!-- /wrapper -->
